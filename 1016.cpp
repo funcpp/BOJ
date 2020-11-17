604a2a167e6d3694ef7d7344362b811ddfeb9479
doftduftsophy <48622976+soaktemp312@users.noreply.github.com>
2020-11-17 13:39:52 +0900

    Update 1016.cpp

--- 1016.cpp
+++ 1016.cpp
@@ -1,32 +1,21 @@
 #include <stdio.h>
-long long numbers[1000001];
-long long prinum[10000];
- 
-int main()
-{
-    long long max, min;
-    long long num;
-    int prinum_num = 0;
-    int count = 0;
-    scanf("%lld %lld", &min, &max);
-    
-    for (long long i = 2; i*i <= max; i++){
-        long long x = min / (i*i);
-        if (min % (i*i) != 0)
-            x++;
-        while (x*(i*i) <= max)
-		{
-            numbers[x*(i*i) - min] = 1;
-            x++;
-        }
- 
-    }
-    for (int i = 0; i <= max - min; i++){
-        if (numbers[i] == 0)
-            count++;
-    }
- 
- 
-    printf("%d\n", count);
-    return 0;
+#include <vector>
+using namespace std;
+int main(){
+	long long min, max, ans=0;
+	scanf("%lld %lld",&min,&max);
+	
+	vector<bool> d = vector<bool>(1000005,0);
+	for(long long i=2;i*i<=max;i++){
+		long long x = min / (i*i);
+		if(min % (i*i)) x++;
+		for(long long j=x*i*i;j<=max;j+=i*i){
+			d[j-min]=1;
+		}
+	}
+	for(long long i=min;i<=max;i++){
+		if(!d[i-min]) ans++;
+	}
+	printf("%lld",ans);
+	return 0;
 }
